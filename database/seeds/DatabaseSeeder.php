5cf2a7cec6bb3dbae95bdc898ef275ca73654de7
unknown <kre8mymedia@gmail.com>
2020-05-19 01:52:15 -0500

    added seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+
+        $x = new App\Post;
+        $x->author_id = 1;
+        $x->title = "What Happened";
+        $x->content = "This is what happend when we did this";
+        $x->save();
+
+        $x = new App\Post;
+        $x->author_id = 2;
+        $x->title = "Something Strang";
+        $x->content = "Something strange happened can't explain it";
+        $x->save();
+
+        $x = new App\Comment;
+        $x->post_id = 1;
+        $x->reply = "Wow that's amazing!";
+        $x->save();
+
+        $x = new App\Comment;
+        $x->post_id = 2;
+        $x->reply = "They did what now?";
+        $x->save();
     }
 }
